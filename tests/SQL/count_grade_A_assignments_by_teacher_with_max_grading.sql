b681d8bc3adfef72df7dd4d793d9cefc053d28db
divyanshu1232003 <d04032004singh@gmail.com>
2024-01-19 13:52:50 +0530

    code added my divyanshu

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,28 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+SELECT
+    t.id AS teacher_id,
+    COUNT(DISTINCT a.id) AS count_grade_A_assignments
+FROM
+    teachers t
+JOIN
+    assignments a ON t.id = a.teacher_id
+WHERE
+    a.grade = 'A'
+GROUP BY
+    t.id
+HAVING
+    COUNT(DISTINCT a.id) = (
+        SELECT
+            COUNT(DISTINCT a2.id)
+        FROM
+            teachers t2
+        JOIN
+            assignments a2 ON t2.id = a2.teacher_id
+        WHERE
+            a2.grade = 'A'
+        GROUP BY
+            t2.id
+        ORDER BY
+            COUNT(DISTINCT a2.id) DESC
+        LIMIT 1
+    );
--- tests/SQL/number_of_assignments_per_state.sql
+++ tests/SQL/number_of_assignments_per_state.sql
@@ -1,1 +1,8 @@
 -- Write query to get number of assignments for each state
+SELECT
+    state,
+    COUNT(id) AS count_assignments
+FROM
+    assignments
+GROUP BY
+    state;
